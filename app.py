a362350dc561ae14071d0641165086c7887943a2
Kirra Hyde <kirra.hyde@gmail.com>
2024-11-12 15:32:06 -0500

    baby api

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
 
-from flask import Flask, request, render_template, redirect, flash, session
+from flask import Flask, request, session, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 
-from models import db, connect_db
+from models import db, connect_db, User, Ingredient
 # TODO: Check order of boiler plate stuff in project that has all this
 
 app = Flask(__name__)
@@ -18,3 +18,35 @@ app.config['SQLALCHEMY_ECHO'] = True
 debug = DebugToolbarExtension(app)
 
 connect_db(app)
+
+@app.post("/api/add_user")
+def add_user():
+    username = request.json["username"]
+    password = request.json["password"]
+    email = request.json["email"]
+
+    user = User(username=username, email=email, password=password)
+    db.session.add(user)
+    db.session.commit()
+
+@app.post("/api/add_ingredient")
+def add_ingredient():
+    username = request.json["username"]
+    name = request.json["name"]
+
+    ingredient = Ingredient(username=username, name=name)
+    db.session.add(ingredient)
+    db.session.commit()
+
+@app.get("/api/get_ingredients")
+def get_ingredient():
+    username = request.json["username"]
+
+    user = User.query.get(username)
+
+    out = []
+    for ingredient in user.ingredients:
+        out.append(ingredient.name)
+
+    return jsonify(out)
+
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 db = SQLAlchemy()
 
@@ -8,3 +9,62 @@ def connect_db(app):
     app.app_context().push()
     db.app = app
     db.init_app(app)
+
+
+class User(db.Model):
+    """User"""
+
+    __tablename__ = "users"
+
+    username = db.Column(
+        db.String(15),
+        primary_key=True
+    )
+
+    email = db.Column(
+        db.String(),
+        nullable=False,
+        unique=True
+    )
+
+    password = db.Column(
+        db.String(25),
+        nullable=False
+    )
+
+    ingredients = db.relationship("Ingredient", backref="user")
+
+
+class Ingredient(db.Model):
+    """A user's ingredient"""
+
+    __tablename__ = "ingredients"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    name = db.Column(
+        db.String(20),
+        nullable=False
+    )
+
+    still_tasty_code  = db.Column(
+        db.String(5),
+        nullable=True
+    )
+
+    best_by_date = db.Column(
+        db.Date,
+        nullable=False,
+        default=datetime.datetime(year=2500, month=12, day=30).date()
+    )
+
+    username = db.Column(
+        db.String(15),
+        db.ForeignKey("users.username"),
+        nullable=False
+    )
+
